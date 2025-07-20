5f8263ff74ded3b0a9085c071f0dc33501bce647
SHIELD SYSTEM <SHIELD SYSTEM@ASUS-PC>
2025-07-21 01:59:06 +0330

    Interface Segregation Principle

---
+++ Solid/Interface Segregation Principle/Interface Segregation Principle/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public interface IPrinter
+{
+    void Print();
+}
+
+public interface IScanner
+{
+    void Scan();
+}
+
+public interface IFax
+{
+    void Fax();
+}
+public class OldPrinter : IPrinter
+{
+    public void Print() { /* ok */ }
+}
+
+
+
+public class Demo
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
